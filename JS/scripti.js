9ee955980d627a4384ee0a400c09f4ae3ac3b8f3
ivan morilla <449925.joan23@fje.edu>
2025-10-21 19:54:40 +0200

    script ivan done

--- JS/scripti.js
+++ JS/scripti.js
@@ -1,3 +1,28 @@
-function  ivan(a) {
+function descifrado_1_ivan(cadena_encriptada) {
+  let resultado = '';
+  let variable_secundaria = '';
 
+  for (let i = 0; i < cadena_encriptada.length; i++) {
+    let letra = cadena_encriptada[i];
+
+    // Comprobamos si la letra es vocal usando comparación directa
+    if (
+      letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u' ||
+      letra === 'A' || letra === 'E' || letra === 'I' || letra === 'O' || letra === 'U'
+    ) {
+      // Si es vocal, añadimos lo acumulado (invertido) y luego la vocal
+      resultado += variable_secundaria.split('').reverse().join('');
+      resultado += letra;
+      variable_secundaria = '';
+    } else {
+        
+      // Si no es vocal, la guardamos en la variable secundaria
+      variable_secundaria += letra;
+    }
+  }
+
+  // Al final, si quedan consonantes, también las invertimos
+  resultado += variable_secundaria.split('').reverse().join('');
+
+  return resultado;
 }
